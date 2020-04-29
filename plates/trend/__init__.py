637e7e52a5a70a66cb1e9fe6d599097adfec0b3a
zizle <zizle_lin@163.com>
2020-04-29 18:25:26 +0800

    作图迁移至客户端

--- plates/trend/__init__.py
+++ plates/trend/__init__.py
@@ -3,7 +3,6 @@
 
 from flask import Blueprint
 from .views import TrendGroupView,UserTrendTableView, TrendTableView
-from .charts import DrawChartView
 
 trend_blp = Blueprint(name='trend', import_name=__name__, url_prefix='/')
 
@@ -11,5 +10,3 @@ trend_blp.add_url_rule('trend/group/', view_func=TrendGroupView.as_view(name='tr
 trend_blp.add_url_rule('user/<int:uid>/trend/table/', view_func=UserTrendTableView.as_view(name='utdtable'))
 
 trend_blp.add_url_rule('trend/table/<int:tid>/', view_func=TrendTableView.as_view(name='trendtable'))
-trend_blp.add_url_rule('trend/<int:tid>/chart/', view_func=DrawChartView.as_view(name='drawchart'))
-
