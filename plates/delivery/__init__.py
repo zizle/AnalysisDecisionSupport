5172bfd9e960d6081c52d250e06cae7bff27f44d
zizle <zizle_lin@163.com>
2020-05-27 17:25:44 +0800

    交割服务

--- plates/delivery/__init__.py
+++ plates/delivery/__init__.py
@@ -7,7 +7,11 @@
 
 from flask import Blueprint
 from .warehouse import WarehouseView, WarehouseVarietyView
+from .variety import VarietyWarehouseView, ProvinceWarehouseView
 
 delivery_blp = Blueprint(name='delivery', import_name=__name__, url_prefix='/')
 delivery_blp.add_url_rule('warehouse/', view_func=WarehouseView.as_view(name='warehouse'))
-delivery_blp.add_url_rule('warehouse/<int:hid>/variety/', view_func=WarehouseVarietyView.as_view(name='whvariety'))
+delivery_blp.add_url_rule('warehouse/<int:hid>/variety/', view_func=WarehouseVarietyView.as_view(name='whvariety'))  # 当前仓库的可交割品种信息
+delivery_blp.add_url_rule('variety/warehouse/', view_func=VarietyWarehouseView.as_view(name='varietywh'))  # 当前品种的所有仓库
+delivery_blp.add_url_rule('province/warehouse/', view_func=ProvinceWarehouseView.as_view(name='provincewh'))  # 当前品种的所有仓库
+
---
+++ plates/delivery/variety.py
@@ -0,0 +1,64 @@
+# _*_ coding:utf-8 _*_
+# Author： zizle
+# Created:2020-05-26
+# ------------------------
+import re
+from flask import request, jsonify, render_template
+from flask.views import MethodView
+from pyecharts.charts import Line
+from db import MySQLConnection
+
+
+class VarietyWarehouseView(MethodView):
+    def get(self):
+        variety_en = request.args.get('v_en', '')
+        try:
+            variety_en = variety_en.upper()
+            if not re.match(r'[A-Z]+', variety_en):
+                raise ValueError('Error')
+        except Exception:
+            return jsonify({'message': '参数错误', 'warehouses': []}), 400
+        # 查询品种下的仓库信息
+        select_statement = "SELECT infowhtb.id,infowhtb.area,infowhtb.name,infowhtb.addr,infowhtb.longitude,infowhtb.latitude," \
+                           "lwhvtb.variety,lwhvtb.variety_en,lwhvtb.linkman,lwhvtb.links,lwhvtb.premium " \
+                           "FROM `link_warehouse_variety` AS lwhvtb " \
+                           "INNER JOIN `info_delivery_warehouse` AS infowhtb " \
+                           "ON lwhvtb.warehouse_id=infowhtb.id AND lwhvtb.variety_en=%s;"
+        db_connection = MySQLConnection()
+        cursor = db_connection.get_cursor()
+        cursor.execute(select_statement, variety_en)
+        query_result = cursor.fetchall()
+        db_connection.close()
+        return jsonify({'message': '获取数据成功!', 'warehouses': query_result})
+
+
+class ProvinceWarehouseView(MethodView):
+    def get(self):
+        # 获取指定地区下的仓库
+        province = request.args.get('province', '')
+        query_statement = "SELECT infotb.id,infotb.area,infotb.name,infotb.addr,infotb.longitude,infotb.latitude," \
+                          "lwhtb.linkman,lwhtb.links,lwhtb.premium, " \
+                          "GROUP_CONCAT(lwhtb.variety) AS delivery_varieties " \
+                          "FROM `info_delivery_warehouse` AS infotb " \
+                          "LEFT JOIN `link_warehouse_variety` AS lwhtb " \
+                          "ON infotb.id=lwhtb.warehouse_id " \
+                          "WHERE infotb.area=%s " \
+                          "GROUP BY infotb.id;"
+        # query_whstatement = "SELECT * " \
+        #                   "FROM `info_delivery_warehouse` " \
+        #                   "where area=%s;"
+        db_connection = MySQLConnection()
+        cursor = db_connection.get_cursor()
+        cursor.execute(query_statement, province)
+        query_result = cursor.fetchall()
+        db_connection.close()
+        # response_result = list()
+        #
+        # for warehouse_item in query_result:
+        #
+        # print(query_result)
+        for item in query_result:
+            print(item)
+        return jsonify({'message': '获取数据成功!', 'warehouses': query_result})
+
+
